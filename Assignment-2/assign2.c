3bea83268ea07f45ec73f9ee4510baec6359f935
Rahul Agarwal <142435356+naiverahul@users.noreply.github.com>
2024-09-25 20:45:01 +0530

    Update assign2.c

--- Assignment-2/assign2.c
+++ Assignment-2/assign2.c
@@ -89,7 +89,7 @@ int main() {
             printf("\n");
         }
 
-        write(STDOUT_FILENO, "Bat's Shell$ ", 12);
+        write(STDOUT_FILENO, "Bat's Shell$ ", 13);
 
         // Input handling
         fgets(input, 1000, stdin);
