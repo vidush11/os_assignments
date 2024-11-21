ff084efa38588273d8c76bc496f11c7059e6aa09
Vidush Jindal <147984123+vidush11@users.noreply.github.com>
2024-11-21 23:26:04 +0530

    Update assign-4.c

--- Assignment-4/assign-4.c
+++ Assignment-4/assign-4.c
@@ -16,15 +16,90 @@
 
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
+
 int fd;
+
+
+typedef struct Memory{
+	void * addr;
+	struct Memory* next;
+	
+
+} Memory;
+
+Memory* init=NULL;
+Memory* curr=NULL;
+
+void* base= NULL;
+int page_size=4096; //4kb page size
+
+void* map_segment(void* addr){
+	Memory* new= (Memory*) malloc(sizeof(Memory));
+	
+	int pages= (int)addr/4096; //this gives the page number for curr proc
+	if (init==NULL){
+		//lseek(fd, 0, SEEK_SET);
+		base=mmap(0, 4096, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE|MAP_FIXED, fd, 0);
+		printf("%p", base);
+		init=new;
+		curr=new;
+		new->addr=base;
+		new->next=NULL;
+	}
+	else{
+		//lseek(fd, pages*4096, SEEK_SET);
+ 		void* addr=mmap(base+pages*4096, 4096, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE|MAP_FIXED, fd, pages*4096);
+		new->addr=addr;
+		new->next=NULL;
+		curr->next=new; 
+		curr=new;
+	}
+	
+	
+	//memcpy(virtual_memory, file_data+phdr.p_offset, phdr.p_memsz);
+
+}
+
+void signal_handler(int sig, siginfo_t *info, void *ucontext) {
+    if (sig==SIGSEGV){
+    	void* addr=info->si_addr;
+    	printf("Segmentation fault at address 0x%x\n",  addr); //this gives the address of segmentation fault
+    	map_segment(addr);
+    }
+}
+
+
+
+void set_custom_signals(){
+    struct sigaction custom_action; // the struct for use in sigaction function
+    struct sigaction default_action;
+    
+    bzero(&custom_action, sizeof(custom_action)); //setting all the bits to 0 in the memory of action to avoid errors
+    bzero(&default_action, sizeof(custom_action));
+    
+    custom_action.sa_handler= &signal_handler;
+    custom_action.sa_flags= SA_SIGINFO;
+
+    sigaction(SIGSEGV, &custom_action, NULL);
+}
+
+
+
+
 
 /*
  * release memory and other cleanups
  */
-void loader_cleanup(int fd, void*mmap_pointer, size_t size) {
+void loader_cleanup(int fd) {
   close(fd);
-  
-  munmap(mmap_pointer, size); //not necessary as when program is done executing it will automatically free allocated space
+  Memory* curr_=init;
+  while (curr_!=NULL){
+  	munmap(curr->addr, 4096);
+  	curr_=curr_->next;
+  	
+  	
+  }
+  //munmap(mmap_pointer, size); //not necessary as when program is done executing it will automatically free allocated space
 }
 
 /*
@@ -37,7 +112,7 @@ int main(int argc, char** argv){
         exit(1);
     }
   
-  int fd = open(argv[1], O_RDONLY);
+  fd = open(argv[1], O_RDONLY);
 
   if (fd==-1) {
       printf("error");
@@ -71,8 +146,15 @@ int main(int argc, char** argv){
   Elf32_Addr entry_address=ehdr.e_entry;
   lseek(fd, e_phoff, SEEK_SET);
   
-  void* virtual_memory;
-  Elf32_Addr offset_from__start;
+  /*
+  void* virtual_memory=mmap(0, 4096*10000, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE, 0, 0);
+  void* virtual_memory2=mmap(0 , 8192, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE, 0, 0);
+  
+  printf("%p\n", virtual_memory);
+  printf("%p\n", virtual_memory2);
+  */
+
+  /*
   for (int ph=0; ph<e_phnum; ph++){
         Elf32_Phdr phdr;
         
@@ -93,11 +175,11 @@ int main(int argc, char** argv){
         }
   }
   
-  
+  */
   
   
   //printf("%d\n",*entry_address);
-  int (*_start)()= (int (*)())(virtual_memory+offset_from__start);
+  int (*_start)()= (int (*)())(entry_address);
 
   // 1. Load entire binary content into the memory from the ELF file.
   // 2. Iterate through the PHDR table and find the section of PT_LOAD 
@@ -108,5 +190,7 @@ int main(int argc, char** argv){
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
   // 6. Call the "_start" method and print the value returned from the "_start"
   int result = _start();
-  printf("User _start return value = %d\n",result);
+  //printf("User _start return value = %d\n",result);
+  loader_cleanup(fd);
+
 }
